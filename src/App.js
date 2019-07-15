068cbb5c52678b8f5f312916ef2de31702c84f82
guilhermerodz <guilherme.rodrigues.profissional@gmail.com>
2019-07-15 14:08:45 -0300

    feat: added global style

--- src/App.js
+++ src/App.js
@@ -6,10 +6,13 @@ import './config/reactotronConfig';
 import Routes from './routes';
 import history from './services/history';
 
+import GlobalStyle from './styles/global';
+
 function App() {
   return (
     <Router history={history}>
       <Routes />
+      <GlobalStyle />
     </Router>
   );
 }
