9ead18d918a675ba12dfba10c3b6074267dfd137
guilhermerodz <guilherme.rodrigues.profissional@gmail.com>
2019-07-15 15:21:48 -0300

    feat: set up redux, redux saga and reducers

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 
 import './config/reactotronConfig';
@@ -6,14 +7,18 @@ import './config/reactotronConfig';
 import Routes from './routes';
 import history from './services/history';
 
+import store from './store';
+
 import GlobalStyle from './styles/global';
 
 function App() {
   return (
-    <Router history={history}>
-      <Routes />
-      <GlobalStyle />
-    </Router>
+    <Provider store={store}>
+      <Router history={history}>
+        <Routes />
+        <GlobalStyle />
+      </Router>
+    </Provider>
   );
 }
 
