4a253d54f847efe70d7b497aa2bcd59907055cac
guilhermerodz <guilherme.rodrigues.profissional@gmail.com>
2019-07-15 16:38:59 -0300

    feat: persistent user session

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Route, Redirect } from 'react-router-dom';
 
-import store from '~/store';
+import { store } from '~/store';
 
 import AuthLayout from '../pages/_layouts/auth';
 import DefaultLayout from '../pages/_layouts/default';
