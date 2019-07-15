075166bbca5ac4ad076ae49b5469290f2a1e903c
guilhermerodz <guilherme.rodrigues.profissional@gmail.com>
2019-07-15 16:26:45 -0300

    feat: added auth

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -2,6 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Route, Redirect } from 'react-router-dom';
 
+import store from '~/store';
+
 import AuthLayout from '../pages/_layouts/auth';
 import DefaultLayout from '../pages/_layouts/default';
 
@@ -10,7 +12,7 @@ export default function RouteWrapper({
   isPrivate,
   ...rest
 }) {
-  const signed = false;
+  const { signed } = store.getState().auth;
 
   if (!signed && isPrivate) {
     return <Redirect to="/" />;
