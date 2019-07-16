d30c98f405ddbbff878af9736cb6a15e09ba1bb2
guilhermerodz <guilherme.rodrigues.profissional@gmail.com>
2019-07-15 21:47:27 -0300

    feat: added profile update action

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Form, Input } from '@rocketseat/unform';
+
+import { updateProfileRequest } from '~/store/modules/user/actions';
 
 import { Container } from './styles';
 
 export default function Profile() {
+  const dispatch = useDispatch();
   const profile = useSelector(state => state.user.profile);
 
-  function handleSubmit(data) {}
+  function handleSubmit(data) {
+    dispatch(updateProfileRequest(data));
+  }
 
   return (
     <Container>
