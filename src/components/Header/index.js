e1077c72030f975b6db5deebbe81540908b224dc
guilhermerodz <guilherme.rodrigues.profissional@gmail.com>
2019-07-15 21:00:55 -0300

    feat: styled notifications

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import Notifications from '~/components/Notifications';
+
 import logo from '~/assets/logo-purple.svg';
 
 import { Container, Content, Profile } from './styles';
@@ -15,6 +17,8 @@ export default function Header() {
         </nav>
 
         <aside>
+          <Notifications />
+
           <Profile>
             <div>
               <strong>Diego Fernandes</strong>
